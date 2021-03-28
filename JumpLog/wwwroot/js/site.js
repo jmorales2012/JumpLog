4077af1450639a36751c2650ad85aabbb3e98c44
Joshua Morales <josh@Joshuas-MacBook-Pro.local>
2021-03-27 22:57:50 -0700

    Added forms, links, dummy data

---
+++ JumpLog/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+var soldier = {
+    lastName: "Morales",
+    firstName: "Josh",
+    rank: "SPC",
+    dodId: "12345678",
+    ssn: "12345678",
+    company: "C",
+    platoon: "1",
+    dateArrived: "3/27/2021"
+};
