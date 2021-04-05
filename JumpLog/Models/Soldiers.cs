21ac0578dbf4cd94718f119c74404a39621d14ff
Joshua Morales <josh@Joshuas-MacBook-Pro.local>
2021-04-04 21:34:50 -0700

    Block pages to logged in users only

---
+++ JumpLog/Models/Soldiers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+namespace JumpLog.Models
+{
+    public static class Soldiers
+    {
+        public static List<Soldier> SoldiersList;
+
+        public static void addSoldier(Soldier tempSoldier)
+        {
+            //Soldier tempSoldier = new Soldier();
+
+            tempSoldier.LastName = "Williams";
+            tempSoldier.FirstName = "Alex";
+            tempSoldier.Rank = "SGT";
+            tempSoldier.DodId = "345678901";
+            tempSoldier.Ssn = "123456789";
+            tempSoldier.Company = "A";
+            tempSoldier.Platoon = "1";
+            tempSoldier.DateArrived = "1/1/2021";
+
+            SoldiersList.Add(tempSoldier);
+        }
+    }
+
+    public class Soldier
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Rank { get; set; }
+        public string DodId { get; set; }
+        public string Ssn { get; set; }
+        public string Company { get; set; }
+        public string Platoon { get; set; }
+        public string DateArrived { get; set; }
+    }
+}
--- JumpLog/wwwroot/js/site.js
+++ JumpLog/wwwroot/js/site.js
@@ -3,13 +3,54 @@
 
 // Write your JavaScript code.
 
-var soldier = {
-    lastName: "Morales",
-    firstName: "Josh",
-    rank: "SPC",
-    dodId: "12345678",
-    ssn: "12345678",
-    company: "C",
-    platoon: "1",
-    dateArrived: "3/27/2021"
-};
+const soldiers = [
+    {
+        lastName: "Morales",
+        firstName: "Josh",
+        rank: "SPC",
+        dodId: "12345678",
+        ssn: "12345678",
+        company: "C",
+        platoon: "1",
+        dateArrived: "3/27/2021"
+    },
+    {
+        lastName: "Dorsey",
+        firstName: "Joe",
+        rank: "SPC",
+        dodId: "87654321",
+        ssn: "87654321",
+        company: "C",
+        platoon: "3",
+        dateArrived: "3/28/2021"
+    }
+]
+
+const addSoldierButton = document.querySelector("#add-soldier-submit")
+const lastNameInput = document.querySelector("#last-name")
+const firstNameInput = document.querySelector("#first-name")
+const rankInput = document.querySelector("#rank")
+const dodIdInput = document.querySelector("#dod-id")
+const ssnInput = document.querySelector("#ssn")
+const companyInput = document.querySelector("#company")
+const platoonInput = document.querySelector("#platoon")
+const dateArrivedInput = document.querySelector("#date-arrived")
+
+addSoldierButton.addEventListener("click", addSoldier)
+
+function addSoldier(e) {
+    e.preventDefault();
+
+    let soldier = {
+        lastName: lastNameInput.value,
+        firstName: firstNameInput.value,
+        rank: rankInput.value,
+        dodId: dodIdInput.value,
+        ssn: ssnInput.value,
+        company: companyInput.value,
+        platoon: platoonInput.value,
+        dateArrived: dateArrivedInput.value
+    }
+    soldiers.push(soldier)
+    console.log(soldier)
+}
